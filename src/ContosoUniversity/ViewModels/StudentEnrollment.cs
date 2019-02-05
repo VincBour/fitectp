3881a54dbd675be3f67c94f72177b25e2f740b89
VincBour <vboursicot@gmail.com>
2019-02-05 14:26:36 +0100

    Version_1 Inscription ok

---
+++ src/ContosoUniversity/ViewModels/StudentEnrollment.cs
@@ -0,0 +1,13 @@
+﻿using ContosoUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.ViewModels
+{
+    public class StudentEnrollment
+    {
+        public Student Student { get; set; }
+    }
+}
