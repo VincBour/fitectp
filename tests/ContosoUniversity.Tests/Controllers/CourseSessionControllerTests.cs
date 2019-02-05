7b7e7344398722d4497431c59b49c44ae7c192eb
VincBour <vboursicot@gmail.com>
2019-02-05 22:59:49 +0100

    Ajout de la base des tests pour CourseSession

---
+++ tests/ContosoUniversity.Tests/Controllers/CourseSessionControllerTests.cs
@@ -0,0 +1,69 @@
+﻿using ContosoUniversity.Controllers;
+using ContosoUniversity.DAL;
+using ContosoUniversity.Models;
+using ContosoUniversity.Tests.Tools;
+using Moq;
+using NUnit.Framework;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ContosoUniversity.Tests.Controllers
+{
+    public class CourseSessionControllerTests : IntegrationTestsBase
+    {
+        private MockHttpContextWrapper httpContext;
+        private StudentController controllerToTest;
+        private SchoolContext dbContext;
+
+        [SetUp]
+        public void Initialize()
+        {
+            //httpContext = new MockHttpContextWrapper();
+            //controllerToTest = new StudentController();
+            //controllerToTest.ControllerContext = new ControllerContext(httpContext.Context.Object, new RouteData(), controllerToTest);
+            //dbContext = new DAL.SchoolContext(this.ConnectionString);
+            //controllerToTest.DbContext = dbContext;
+        }
+
+        [Test]
+        public void Create_ValidCourseSession_Success()
+        {
+            Assert.True(true);
+        }
+        [Test]
+        public void Create_DayOfWeek_equal_DateChoose_Success()
+        {
+            Assert.True(true);
+        }
+        [Test]
+        public void Create_HourStart_Between8and18_Success()
+        {
+            Assert.True(true);
+        }
+        [Test]
+        public void Create_HourEnd_Between9and19_Success()
+        {
+            Assert.True(true);
+        }
+        [Test]
+        public void Create_HourStart_SmallestThan_HourEnd_Success()
+        {
+            Assert.True(true);
+        }
+        [Test]
+        public void Create_InstructorHadAlreadyACourse_Success()
+        {
+            Assert.True(true);
+        }
+        [Test]
+        public void Create_CourseNotWithThisInstructor_Success()
+        {
+            Assert.True(true);
+        }
+
+
+
+
+    }
+}
