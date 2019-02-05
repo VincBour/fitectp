04a28e479a23ef03b9f6c3c4867d25bca4e556e4
VincBour <vboursicot@gmail.com>
2019-02-05 15:14:05 +0100

    Creation de l'API Get pour Student

---
+++ src/ContosoUniversity/ViewModels/StudentApiViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ContosoUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.ViewModels
+{
+    public class StudentApiViewModel
+    {
+        public IEnumerable<Student> Students { get; set; }
+        public IEnumerable<Enrollment> Enrollments {get;set;}
+    }
+}
