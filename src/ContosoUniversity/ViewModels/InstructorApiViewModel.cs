d732be447ba94502c1eefc058f584091714803ce
VincBour <vboursicot@gmail.com>
2019-02-07 17:41:52 +0100

    Comit APi US1 AND 2 ok

---
+++ src/ContosoUniversity/ViewModels/InstructorApiViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using ContosoUniversity.Enum;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.ViewModels
+{
+    /// <summary>
+    /// This ViewModel is used for instructor API service  
+    /// One instructor can dispense a list of coursesessions
+    /// </summary>
+
+    public class InstructorApiViewModel
+    {
+        public int InstructorID { get; set; }
+
+        public List<CourseSessionApiViewModel> CourseSessionApiViewModels { get; set; }
+
+    }
+}
+
